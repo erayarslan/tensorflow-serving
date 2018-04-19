f7b2a5a50dd5125efbf494c1e07604db86a4e06e
Hans Pabst <hans.pabst@intel.com>
2018-04-19 10:57:49 +0200

    Adjusted build.

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ bazel build --verbose_failures \
   --copt=-O2 --copt=-fopenmp-simd --copt=-DLIBXSMM_OPENMP_SIMD \
   --define tensorflow_xsmm=1 --define tensorflow_xsmm_convolutions=1 \
   --action_env TF_REVISION="master" \
-  --copt=-mfma --copt=-mavx2 --copt=-mavx512f --copt=-mavx512cd \
-  --copt=-mavx512bw --copt=-mavx512vl --copt=-mavx512dq \
+  --copt=-mfma --copt=-mavx2 \
   tensorflow_serving/...
 
