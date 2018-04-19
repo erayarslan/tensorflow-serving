4e25ddb0348c6ae2709037a26640d418401e8193
Hans Pabst <hans.pabst@intel.com>
2018-04-19 09:37:54 +0200

    Pull master revision.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ source /nfs/home/tools/gcc/setenv-7.3.0.sh
 bazel build --verbose_failures \
   --copt=-O2 --copt=-fopenmp-simd --copt=-DLIBXSMM_OPENMP_SIMD \
   --define tensorflow_xsmm=1 --define tensorflow_xsmm_convolutions=1 \
-  --action_env TF_REVISION="b2329a01e823db74e8bf2b5109b5d59ef1534e88" \
+  --action_env TF_REVISION="master" \
   --copt=-mfma --copt=-mavx2 --copt=-mavx512f --copt=-mavx512cd \
   --copt=-mavx512bw --copt=-mavx512vl --copt=-mavx512dq \
   tensorflow_serving/...
