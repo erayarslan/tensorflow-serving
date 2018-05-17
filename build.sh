84c9823a63331c649df49729aac00cc5aa5a901f
Hans Pabst <hans.pabst@intel.com>
2018-05-17 18:11:55 +0200

    Updated script, which sources the build environment.

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 source /nfs/home/tools/bazel/enable
-source /nfs/home/tools/gcc/setenv-7.3.0.sh
+source /nfs/home/tools/gcc/setenv.sh
 
 bazel build --verbose_failures \
   --copt=-O2 --copt=-fopenmp-simd --copt=-DLIBXSMM_OPENMP_SIMD \
   --define tensorflow_xsmm=1 --define tensorflow_xsmm_convolutions=1 \
   --action_env TF_REVISION="master" \
   --copt=-mfma --copt=-mavx2 \
-  tensorflow_serving/...
-
+  tensorflow_serving/... tensorflow_serving/apis/...
