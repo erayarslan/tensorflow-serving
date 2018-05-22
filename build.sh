926453aaed238d45aae6805ba23209eb2bdfb932
Hans Pabst <hans.pabst@intel.com>
2018-05-22 11:45:31 +0200

    Comment about NDEBUG.

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 source /nfs/home/tools/bazel/enable
 source /nfs/home/tools/gcc/setenv.sh
 
-bazel build --verbose_failures \
+FLAGS="--copt=-mfma --copt=-mavx2 --copt=-mavx512f --copt=-mavx512cd --copt=-mavx512bw --copt=-mavx512vl --copt=-mavx512dq"
+FLAGS="--copt=-mfma --copt=-mavx2"
+
+# NDEBUG preprocessor symbol (as implied by "-c opt") "allows" for AVX-512
+# However, an Eigen assertion (hidden by NDEBUG) is not present with AVX2.
+#
+bazel build --verbose_failures -c opt --copt=-DNDEBUG \
   --copt=-O2 --copt=-fopenmp-simd --copt=-DLIBXSMM_OPENMP_SIMD \
   --define tensorflow_xsmm=1 --define tensorflow_xsmm_convolutions=1 \
-  --action_env TF_REVISION="master" \
-  --copt=-mfma --copt=-mavx2 \
-  tensorflow_serving/... tensorflow_serving/apis/...
+  --action_env TF_REVISION="master" ${FLAGS} \
+  tensorflow_serving/...
+
